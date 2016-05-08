310f289d06f2af9b8828f33d6ebff90effa3a166
Heo <duheo@APSEONOTE094.local>
2016-05-08 23:52:53 +0900

    1 turn available

--- server/lasvegas.js
+++ server/lasvegas.js
@@ -4,54 +4,79 @@ import { GameRoom } from './room'
 export class LasVegas extends GameRoom
 {
   get TotalRound() { return 4; }
-  get TableCount() { return 6; }
+  get TableNames() { return [1, 2, 3, 4, 5, 6]; }
   get DiceCount() { return 8; }
   get MaxPrize() { return 5; }
   
   constructor(server) {
     super(server);
     
+    this.gameOver = () => {
+      console.log('Running gameOver()');
+    }
+    
+    this.nextPlayer = () => {
+      console.log('Running nextPlayer()');
+      
+      if (this.currentPlayer) {
+        let nextPlayer = (this.players.findIndex(player => player == this.currentPlayer) + 1) % this.players.count;
+        console.log(`nextPlayer = ${nextPlayer}`);
+        let nextCandidates = this.players.slice(0, nextPlayer).concat(this.players.slice(nextPlayer)).filter(player => player.diceCount > 0);
+        console.log(`nextCandidates = ${nextCandidates.join(',')}`);
+        this.currentPlayer = (nextCandidates.count > 0) ? nextCandidates[0] : null;
+        console.log(`currentPlayer = ${this.currentPlayer}`);
+      } else {
+        this.currentPlayer = this.players[0];
+      }
+            
+      if (this.currentPlayer) {
+        this.broadcast({ cmd: 'nextPlayer', player: this.currentPlayer, game: this.toJson() });
+        this.onAllReady = this.roll;
+      } else {
+        this.broadcast({ cmd: 'result', game: this.toJson() });
+        if (++this.currentRound == this.TotalRound)
+          this.onAllReady = this.gameOver;
+        else
+          this.onAllReady = this.initRound;
+      }
+    }
+    
     this.roll = () => {
       console.log('Running roll()');
       function* rollDices(diceCount, TableCount) {
-        while (--diceCount >= 0) 
+        while (--diceCount >= 0) {
           yield Math.floor(Math.random() * TableCount) + 1;
+        }
       }
-      this.currentPlayer.dices = [...rollDices(this.currentPlayer.diceCount, this.TableCount)]; 
+      this.currentPlayer.dices = [...rollDices(this.currentPlayer.diceCount, this.TableNames.length)].sort((lhs, rhs) => lhs - rhs); 
       console.log(`Player ${this.currentPlayer.id} rolls ${this.currentPlayer.dices}`);
-      this.send({ cmd: 'roll', dices: this.currentPlayer.dices }, this.currentPlayer.id);
-      this.onAllReady = this.dices;
+      this.send({ cmd: 'roll', player: this.currentPlayer }, this.currentPlayer.id);
     };
     
     this.initRound = () => {
       console.log('Running initRound()');
-      var round = { tables: [] };
+      var round = { tables: {} };
       this.rounds.push(round);
       
-      function* setupTables(prizes, MaxPrize, TableCount) {
-        function* setupTable(prizes, MaxPrize) {
-          var totalPrize = 0; 
-          do {
-            var prize = prizes.pop();
-            yield prize;
-            totalPrize += prize;
-          } while (totalPrize < MaxPrize);
-        }
-        
-        while (--TableCount >= 0) {
-          var table = { prizes: [...setupTable(prizes, MaxPrize)] };
-          table.prizes.sort((lhs, rhs) => rhs - lhs);
-          yield table;
+      function* setupTable(prizes, MaxPrize) {
+        var totalPrize = 0; 
+        do {
+          var prize = prizes.pop();
+          yield prize;
+          totalPrize += prize;
+        } while (totalPrize < MaxPrize);
+      }
+      for (var name of this.tables) {
+        round.tables[name] = {
+          name: name,
+          prizes: [...setupTable(this.prizes, this.MaxPrize)].sort((lhs, rhs) => rhs - lhs)
         }
       }
-      round.tables = [...setupTables(this.prizes, this.MaxPrize, this.TableCount)]; 
       
       this.players.forEach(player => player.diceCount = 8);
       this.currentRound = round;
-      this.currentPlayer = this.currentPlayer || this.players[0];
-      console.log(`Current Player: ${JSON.stringify(this.currentPlayer)}`);
       this.broadcast({ cmd: 'initRound', game: this.toJson() });
-      this.onAllReady = this.roll;
+      this.onAllReady = this.nextPlayer;
     };
     
     this.init = () => {
@@ -65,6 +90,7 @@ export class LasVegas extends GameRoom
       });
       shuffle(this.prizes);
       
+      this.tables = this.TableNames;        
       this.broadcast({ cmd: 'gameStart', game: this.toJson() });
       this.onAllReady = this.initRound;        
     };
@@ -74,6 +100,17 @@ export class LasVegas extends GameRoom
   
   static createGame(server) { return new LasVegas(server); }
 
+  invest(id, message) {
+    if (this.currentPlayer.id == id) {
+      let funds = this.currentPlayer.dices.filter(dice => dice == message.table);
+      console.log(`Dices [${this.currentPlayer.dices.join(',')}] invest [${funds.join(',')}]`);
+      this.currentPlayer.diceCount -= this.currentPlayer.dices.count;
+      delete this.currentPlayer.dices;
+      this.broadcast({ cmd: 'invested', player: this.currentPlayer, table: message.table, dices: funds, game: this.toJson() });
+      this.onAllReady = this.nextPlayer;
+    }  
+  }
+  
   gameover() {
     this.broadcast({ cmd: 'result' });
   }
